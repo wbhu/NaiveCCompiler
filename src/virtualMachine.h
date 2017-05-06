1e298b4a4ffe0f6fc694aedb042e36bd5ed59b09
cris <huwenbo@mail.dlut.edu.cn>
2017-05-06 13:40:05 +0800

    sync

---
+++ src/virtualMachine.h
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author:		胡文博
+ * @email:		huwenbo@mail.dlut.edu.cn
+ * @dateTime:		2017-05-05 20:19:03
+ * @description:
+ */
+
+#include "iostream"
+#include <list>
+
+
+// instructions
+enum { LEA , IMM , JMP , CALL, JZ  , JNZ , ENT , ADJ , LEV , LI  , LC  , SI  , SC  , PUSH,
+       OR  , XOR , AND , EQ  , NE  , LT  , GT  , LE  , GE  , SHL , SHR , ADD , SUB , MUL , DIV , MOD ,
+       OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT
+     };
+
+
+
+class virtualMachine
+{
+private:
+	int poolsize;         // default size of text/data/stack
+	int *pc, *bp, *sp, ax, cycle; // virtual machine registers
+	// list<int> instructionStream;
+public:
+	virtualMachine();
+	~virtualMachine();
+	int run(list<int> &instructionStream );
+	void loadInstruction(list<int> instructionStream);
+
+};
